346c75c771ce2ee5397b96703bb42a64c328303e
ICARO DAVILA SANTOS <icaro.davila.santos@gmail.com>
2023-07-26 15:27:10 -0300

    Add files via upload

---
+++ URI 1116/Program.cs
@@ -0,0 +1,27 @@
+﻿class URI {
+
+    static void Main(string[] args) { 
+
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++) 
+        {
+            string[] dados = Console.ReadLine().Split(' ');
+            int x = int.Parse(dados[0]);
+            int y = int.Parse(dados[1]);
+
+            if (y == 0)
+            {
+                Console.WriteLine("divisao impossivel");
+            }
+            else
+            {
+                double divisao = (double)x / y;
+
+                Console.WriteLine($"{divisao}");
+            }
+        }
+
+    }
+
+}
