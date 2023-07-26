f2c65e990152d614dda4fe4ea298645a2bc7905b
IcaroD <icaro.davila.santos@gmail.com>
2023-07-26 20:05:19 -0300

    nova

---
+++ BEE 2048/Program.cs
@@ -0,0 +1,38 @@
+﻿class URI {
+
+    static void Main(string[] args) { 
+
+        int N = int.Parse(Console.ReadLine());
+
+        int[] vet = new int[N + 1];
+
+        int soma = -1;
+        vet[0] = 1;
+        for (int i = 1; i <= N; i++)
+        {
+            vet[i] = int.Parse(Console.ReadLine());
+
+            if (vet[i] == vet[i-1] && soma <= 7)
+            {
+                soma += vet[i];
+            }
+            else if (vet[i] != vet[i-1] && vet[i-1] == vet[i-2] && soma <= 7)
+            {
+                soma += vet[i-1];
+            }
+        }
+
+        if (soma > 7)
+            {
+                soma = 7;
+            }
+        else if (soma < 3)
+        {
+            soma = 3;
+        }
+
+        Console.WriteLine(soma);
+
+    }
+
+}
---
+++ BEE 2374/Program.cs
@@ -0,0 +1,14 @@
+﻿class URI {
+
+    static void Main(string[] args) { 
+
+        int N = int.Parse(Console.ReadLine());
+        int M = int.Parse(Console.ReadLine());
+
+        int dif = N - M;
+
+        Console.WriteLine(dif);
+
+    }
+
+}
