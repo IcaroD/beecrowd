65ec8772a2cb4f382b048f5b0022f168d0ec41e6
IcaroD <icaro.davila.santos@gmail.com>
2023-07-26 23:24:31 -0300

    ainda 45%

--- BEE 2048/Program.cs
+++ BEE 2048/Program.cs
@@ -12,12 +12,14 @@
         }
 
         int soma = 0;
-        for (int i = 1; i < N; i++)
+        int atual = vet[0];
+        for (int i = 0; i < N; i++)
         {
-            if (vet[i] != vet[i-1])
+            if (vet[i] != atual)
             {
                 soma += vet[i];
             }
+            atual = vet[i];
         }
         
         if (soma > 7)
