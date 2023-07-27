db760aed1b8d5f927cf8dae82c87ada7ced05d43
IcaroD <icaro.davila.santos@gmail.com>
2023-07-26 21:00:38 -0300

    45%

--- BEE 2048/Program.cs
+++ BEE 2048/Program.cs
@@ -6,29 +6,23 @@
 
         int[] vet = new int[N + 1];
 
-        int soma = -1;
-        vet[0] = 1;
-        for (int i = 1; i <= N; i++)
+        for (int i = 0; i < N; i++) 
         {
             vet[i] = int.Parse(Console.ReadLine());
+        }
 
-            if (vet[i] == vet[i-1] && soma <= 7)
+        int soma = 0;
+        for (int i = 1; i < N; i++)
+        {
+            if (vet[i] != vet[i-1])
             {
                 soma += vet[i];
             }
-            else if (vet[i] != vet[i-1] && vet[i-1] == vet[i-2] && soma <= 7)
-            {
-                soma += vet[i-1];
-            }
         }
-
+        
         if (soma > 7)
-            {
-                soma = 7;
-            }
-        else if (soma < 3)
         {
-            soma = 3;
+            soma = 7;
         }
 
         Console.WriteLine(soma);
